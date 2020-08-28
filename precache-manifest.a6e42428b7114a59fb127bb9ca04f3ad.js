75806d9bf330bd8bec7e4395b56528761db7a3a0
pasta777 <58119607+pasta777@users.noreply.github.com>
2020-08-28 14:21:05 +0200

    Updates

---
+++ precache-manifest.a6e42428b7114a59fb127bb9ca04f3ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9accaaf57f5685973c8a8082f34dccdb",
+    "url": "/robolist/index.html"
+  },
+  {
+    "revision": "63232b95201ef80b5f3d",
+    "url": "/robolist/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "677c50e6872f4767a151",
+    "url": "/robolist/static/css/main.b8f1e001.chunk.css"
+  },
+  {
+    "revision": "63232b95201ef80b5f3d",
+    "url": "/robolist/static/js/2.efec3637.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robolist/static/js/2.efec3637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "677c50e6872f4767a151",
+    "url": "/robolist/static/js/main.3ce21619.chunk.js"
+  },
+  {
+    "revision": "25cdf5d5035045c06208",
+    "url": "/robolist/static/js/runtime-main.4b3163a9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robolist/static/media/SEGA.12a3db8b.woff"
+  }
+]);
