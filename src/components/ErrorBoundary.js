60d448219865a6e2d8f9412a0d221164ce56c53e
pasta777 <58119607+pasta777@users.noreply.github.com>
2020-08-28 14:26:05 +0200

    initial commit

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    }
+  }
+  componentDidCatch() {
+    this.setState({hasError: true});
+  }
+  render() {
+    if(this.state.hasError) {
+      return <h1>There is an error!</h1>
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
---
+++ src/containers/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import ErrorBoundary from "../components/ErrorBoundary";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+  onSearchChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboList</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
+
+export default App;
